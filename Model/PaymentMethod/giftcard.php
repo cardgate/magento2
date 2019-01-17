b585954fd11757f880ccf62b4393df9375387715
richard <richard@richard-imac2.dbs.dbcorp.nl>
2019-01-17 16:04:16 +0100

    New payment method: Gift Card. New payment method logo's.

---
+++ Model/PaymentMethod/giftcard.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Copyright (c) 2018 CardGate B.V.
+ * All rights reserved.
+ * See LICENSE for license details.
+ */
+namespace Cardgate\Payment\Model\PaymentMethod;
+
+/**
+ * Gift Card class.
+ * @author DBS B.V.
+ * @package Magento2
+ */
+class giftcard extends \Cardgate\Payment\Model\PaymentMethods {
+
+	/**
+	 * Payment method code
+	 *
+	 * @var string
+	 */
+	protected $_code = 'cardgate_giftcard';
+
+}
