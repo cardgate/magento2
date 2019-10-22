068fdda4deaf5153ee084889123e3a0c8ac0f4ae
richard <richard@cardgate.com>
2019-10-22 16:25:15 +0200

    New payment method: onlineueberweisen.

---
+++ Model/PaymentMethod/onlineueberweisen.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Copyright (c) 2018 CardGate B.V.
+ * All rights reserved.
+ * See LICENSE for license details.
+ */
+namespace Cardgate\Payment\Model\PaymentMethod;
+
+/**
+ * OnlineÜberweisen class.
+ * @author DBS B.V.
+ * @package Magento2
+ */
+class onlineueberweisen extends \Cardgate\Payment\Model\PaymentMethods {
+
+	/**
+	 * Payment method code
+	 *
+	 * @var string
+	 */
+	protected $_code = 'cardgate_onlineueberweisen';
+
+}
